9d2255ce7799e681bccbbc2bda07b235b61dadf7
awttt <350944364@qq.com>
2020-07-11 20:04:45 +0800

    基础布局

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
       <main id="main">
         <router-view/>
       </main>
-
     <Footer id="footer"></Footer>
   </div>
 </template>
@@ -31,9 +30,22 @@ font-family: 'Avenir',Helvetica,Arial,sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
-#nav {
+#header {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+}
+#footer{
+  border: 1px solid purple;
 
 }
+  #main{
+    flex-grow: 1;
+    overflow: auto
+  }
 </style>
