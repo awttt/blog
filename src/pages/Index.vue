8d433af58c2aab2643cec6a34487012f4ef423a2
awttt <350944364@qq.com>
2020-07-11 16:01:55 +0800

    封装blog.js auth.js

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,11 @@
 
 <script>
   import request from "../helpers/request"
+  import auth from "../api/auth"
+  import blog from "../api/blog"
   window.request = request
+  window.auth = auth
+  window.blog = blog
   export default {
     methods: {
       open1() {
